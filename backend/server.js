0f8e98a6e1f2ff75dff88717856e485a51c76be0
Deepak Kumar <deepakkumar23666@gmail.com>
2026-02-07 14:35:49 +0530

    Initialize Express server with MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/smartcareerai")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("SmartCareerAI Backend Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
